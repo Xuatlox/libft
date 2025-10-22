8a84501b0790eaf4eec8be90741ed35a8dd7a7e3
ansimonn <ansimonn@student.42angouleme.fr>
2025-10-22 17:18:58 +0200

    calloc updated

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:35:52 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/22 10:23:07 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/22 17:17:20 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*res;
 	size_t	i;
 
-	if (nmemb == 0)
-		return (malloc(0));
 	res = malloc(nmemb * size);
 	if (!res)
 		return (NULL);
