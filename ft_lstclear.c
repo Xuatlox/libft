25655c102dd2dc703b83304442e69fd155d7a5b5
ansimonn <ansimonn@student.42angouleme.fr>
2025-10-22 18:11:01 +0200

    lstclear updated

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 11:57:21 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/22 12:09:19 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/22 18:10:47 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
+	if (!del || !*lst)
+		return ;
 	while (*lst)
 	{
 		tmp = *lst;
