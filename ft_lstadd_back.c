728f530a27d1dfbb81e570e624c8c6b5b5750a56
ansimonn <ansimonn@student.42angouleme.fr>
2025-10-22 18:08:20 +0200

    lstadd_front & lstadd_back updated

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 11:31:59 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/22 11:35:53 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/22 18:05:15 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	if (!new)
+		return ;
 	if (!*lst)
 		*lst = new;
 	else
--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 11:07:40 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/22 18:02:26 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/22 18:07:36 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!lst || !new)
+	if (!new)
 		return ;
+	if (!*lst)
+		*lst = new;
 	new->next = *lst;
 	*lst = new;
 }
