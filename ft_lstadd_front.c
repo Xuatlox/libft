2e6eb5f42a17424fad0717ed895fe5c4aa78e3c1
ansimonn <ansimonn@student.42angouleme.fr>
2025-10-23 09:48:47 +0200

    lstadd_front correction

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 11:07:40 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/22 18:07:36 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/23 09:39:12 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	if (!new)
 		return ;
 	if (!*lst)
+	{
 		*lst = new;
+		return ;
+	}
 	new->next = *lst;
 	*lst = new;
 }
