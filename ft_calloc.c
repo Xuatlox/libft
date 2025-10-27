9341bb91d25a5c7c2572c9e06b7382ce75dcfdd1
ansimonn <ansimonn@student.42angouleme.fr>
2025-10-27 10:47:36 +0100

    calloc update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,27 +6,21 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 14:35:52 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/23 17:07:11 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/10/27 10:47:01 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
-	size_t	i;
 
-	if (nmemb * size > 65535)
+	if (size != 0 && nmemb * size / size != nmemb)
 		return (NULL);
 	res = malloc(nmemb * size);
 	if (!res)
 		return (NULL);
-	i = 0;
-	while (i < nmemb * size)
-	{
-		((char *)res)[i] = 0;
-		i++;
-	}
+	ft_bzero(res, nmemb * size);
 	return (res);
 }
